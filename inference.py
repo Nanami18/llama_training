73082e8a6e3537150b1bc74d4b755d3b41f463bb
Thomas <nanami17@cs.washington.edu>
2023-06-01 14:11:29 -0700

    fix bug

--- inference.py
+++ inference.py
@@ -72,4 +72,4 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using device:", device)
 
-    train_model(args, device)
+    inference(args, device)
